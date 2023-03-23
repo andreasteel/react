9e81988869e3bec67af58a7e76c09df25b50cc78
andreasteel <102710199+andreasteel@users.noreply.github.com>
2023-03-22 23:16:19 -0600

    kk

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const teamNames = [
+  {"tid": 159, "cid": 0, "did": 0, "school": "Boston College", "name": "Eagles", "abbrev": "BC", "pop": 161, "city": "Chestnut Hill", "state": "MA", "latitude": 42.330, "longitude": -71.166},
+  {"tid": 105, "cid": 0, "did": 0, "school": "Clemson", "name": "Tigers", "abbrev": "CLEM", "pop": 215, "city": "Clemson", "state": "SC", "latitude": 34.678, "longitude": -82.839},
+  {"tid": 109, "cid": 0, "did": 0, "school": "Florida State", "name": "Seminoles", "abbrev": "FSU", "pop": 211, "city": "Tallahassee", "state": "FL", "latitude": 30.442, "longitude": -84.298},
+];
+
+function team extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>school</h3>
+        <h3>name</h3>
+        <h3>city</h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      <team/>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to my page (Andrea)</h1>
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to March Madness</h1>
+        <TeamList/>
       </header>
     </div>
+
   );
 }
 
